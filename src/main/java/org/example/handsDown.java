a8f3bf10d72cba09bafdefc36df768ab2194c0b2
Big-juju <qhrrl3891@gmail.com>
2023-05-14 16:04:22 +0900

    Initial commit

---
+++ src/main/java/org/example/handsDown.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import javafx.stage.Stage;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+
+public class handsDown extends ListenerAdapter{
+    public static void main(String[] args) throws LoginException {
+    {
+        JDA jda = JDABuilder.createDefault("OTE2OTQ0Nzk4NDA4MTM0NzE2.Gujl4p.IQzPAAk1ovzpM0NrSQLHisUctfug7HvdmauiyY").build();
+        jda.getPresence().setStatus(OnlineStatus.ONLINE);
+        jda.getPresence().setActivity(Activity.playing("삐그덕 손 내려 봇"));
+
+        jda.addEventListener(new handsDown());
+    }
+}
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String[] args = event.getMessage().getContentRaw().split("ping ",3);
+        String prefix = "ping";
+
+        if (!event.getAuthor().isBot()) {
+            if (args[0].equals(prefix)) {
+                if(args.length == 1) {
+                    event.getChannel().sendMessage("ping <code>").queue();
+                } else {
+                    //String content = "```\n" + event.getMessage().getContentRaw() + "\n```";
+                    for(int i = 1; i < args.length-1; i++){
+                        event.getChannel().sendMessage("```\n"+args[i]+ "\n```").queue();
+                    }
+                }
+            }
+        }
+    }
+}
+
